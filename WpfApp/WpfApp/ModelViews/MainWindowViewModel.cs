dcfadc086303cd5ab435e89ff3b63a137c344e66
Qwertty <kamilwagrowski83@gmail.com>
2023-04-13 14:33:47 +0200

    model

---
+++ WpfApp/WpfApp/ModelViews/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp.Commands;
+ 
+
+namespace WpfApp.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            IloczynCommand = new RelayCommand(iloczyn);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private string arg1;
+        public string Arg1
+        {
+            get { return arg1; }
+            set
+            {
+                arg1 = value;
+                OnPropertyChanged(nameof(Arg1));
+            }
+        }
+        public ICommand IloczynCommand { get; set; }
+        public void iloczyn(Object obj)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
--- WpfApp/WpfApp/Views/MainWindow.xaml.cs
+++ WpfApp/WpfApp/Views/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() 
         {
             InitializeComponent();
         }
+
+       
     }
 }
